2931c06dcd93d6c6905b4d1ac4dab80b13fe8d61
ocb <1251880999@qq.com>
2023-04-25 00:15:47 +0800

    Initial Commit

---
+++ 微博项目练习/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  微博项目练习
+//
+//  Created by 云动家 on 2023/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 微博项目练习UITests/______UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ______UITestsLaunchTests.swift
+//  微博项目练习UITests
+//
+//  Created by 云动家 on 2023/4/25.
+//
+
+import XCTest
+
+final class ______UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
